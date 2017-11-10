1027204ae8643658da802d0851a3dee2f6d9c38d
Thomas Prosser <tp@thomit.com>
2017-11-10 19:23:22 +0100

    Initial Commit

---
+++ Example/Pods/Target Support Files/Pods-TICoreNFCExtensions_Tests/Pods-TICoreNFCExtensions_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TICoreNFCExtensions_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_TICoreNFCExtensions_Tests
+@end
---
+++ Example/Pods/Target Support Files/TICoreNFCExtensions/TICoreNFCExtensions-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TICoreNFCExtensions : NSObject
+@end
+@implementation PodsDummy_TICoreNFCExtensions
+@end
